1d2829d08e3e1354848708221cc5d68e75d9c0ce
Alex Bykov <bober@Mac-mini.local>
2017-02-12 21:50:55 +0300

    error modular

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import errors from "./errors";
+import error from "./errors";
 
 class Watchable {
 
@@ -92,32 +92,7 @@ class Watchable {
     static invokeError(errorcode){
         throw new Error(Watchable.error(errorcode));
     }
-    static error(type) {
-        let message;
 
-        switch (type) {
-
-            case ("typeMismatch"):
-                message = "value of Watchable should match its type.\
-            \nIf you do not want strict typing then omit 'type' argument";
-                break;
-
-            case ("noName"):
-                message = "watchable's name should be provided as a first argument.\
-            \nUsing watchable 'name' argument same as variable name is a good practice.";
-                break;
-
-            case("nameMustBeString"):
-                message = "watchable's name should always be a string.\
-                \nMay be you forgot to add the name argument.";
-                break;
-
-            default:
-                break;
-        }
-        console && console.warn("%c Watchable error: %c " + message, "color: white; background-color: navy", "");
-throw new Error();
-}
 
     static render(el) {
         return el.innerHTML = eval(el.getAttribute('watchable') + '.value');
@@ -132,5 +107,6 @@ throw new Error();
 }
 
 Watchable.watchables = [];
+Watchable.error = error;
 
 module.exports = Watchable;
