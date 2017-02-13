cbb9646dfba62ec49fc178d92b40839e2022b670
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2017-02-13 22:58:34 +0300

    Fix singlequotes to be doublequotes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
-const Watchable = require('../dist/main.bundle.js');
-const chai = require('chai');
+const Watchable = require("../dist/main.bundle.js");
+const chai = require("chai");
 const expect = chai.expect;
 
-describe('Sanity check', () => {
-    it('should be defined', () => {
-        expect(Watchable).not.to.be.an('undefined');
+describe("Sanity check", () => {
+    it("should be defined", () => {
+        expect(Watchable).not.to.be.an("undefined");
     });
 });
