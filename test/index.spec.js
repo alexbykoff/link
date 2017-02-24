880ddafdf1338ebe7110b970e9a4d04706008731
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2017-02-24 22:29:56 +0300

    trying to make tests pass

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,22 @@
 const Watchable = require("../src/index.js");
 const chai = require("chai");
 const expect = chai.expect;
+const assert = chai.assert;
+
+require('jsdom-global')();
 
 describe("Sanity check", () => {
     it("should be a function", () => {
         expect(Watchable).to.be.a("function");
+    });
+});
+
+describe("setType", () => {
+    it("should return a string when string passed as an argument", () => {
+        const W = new Watchable("Test", {
+            value: "test",
+            type: "string"
+        });
+        assert.equal("string", W.type());
     });
 });
